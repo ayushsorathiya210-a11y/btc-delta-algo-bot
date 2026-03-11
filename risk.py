999cda0f80ea906d316b1230dcd0f1c2981f195e
ayushsorathiya210-a11y <ayushsorathiya210@gmail.com>
2026-03-11 20:33:03 +0530

    Update risk.py

--- risk.py
+++ risk.py
@@ -1,17 +1,22 @@
 from config import BASE_RISK, MIN_RISK
 
+
 def dynamic_risk(current_dd, trades, atr_percentile):
 
     if current_dd < 0.10:
         risk = BASE_RISK
+
     elif current_dd < 0.20:
         risk = BASE_RISK * 0.75
+
     elif current_dd < 0.30:
         risk = BASE_RISK * 0.5
+
     else:
         risk = MIN_RISK
 
     if len(trades) >= 3:
+
         if trades[-1] < 0 and trades[-2] < 0 and trades[-3] < 0:
             risk *= 0.5
 
