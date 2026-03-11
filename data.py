6e5cb015e58a0b280ce791948a7d264596e47c09
ayushsorathiya210-a11y <ayushsorathiya210@gmail.com>
2026-03-11 17:08:02 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,23 +1,27 @@
 import requests
 import pandas as pd
+import time
 from config import SYMBOL
 
 BASE_URL = "https://api.delta.exchange/v2/history/candles"
 
 def fetch_candles(resolution, limit=400):
 
+    end = int(time.time())
+    start = end - (limit * resolution_to_seconds(resolution))
+
     params = {
         "symbol": SYMBOL,
         "resolution": resolution,
-        "limit": limit
+        "start": start,
+        "end": end
     }
 
     try:
         r = requests.get(BASE_URL, params=params, timeout=10)
         data = r.json()
 
-        # 🔹 Check API response
-        if "result" not in data:
+        if not data.get("success"):
             print("⚠️ Candle API Error:", data)
             return pd.DataFrame()
 
@@ -36,3 +40,19 @@ def fetch_candles(resolution, limit=400):
     except Exception as e:
         print("⚠️ Candle Fetch Error:", e)
         return pd.DataFrame()
+
+
+def resolution_to_seconds(res):
+
+    mapping = {
+        "1m": 60,
+        "5m": 300,
+        "15m": 900,
+        "30m": 1800,
+        "1h": 3600,
+        "2h": 7200,
+        "4h": 14400,
+        "1d": 86400
+    }
+
+    return mapping.get(res, 900)
