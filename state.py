546029e535bfe3f2b74df75ec2c08ff82a0d076a
ayushsorathiya210-a11y <ayushsorathiya210@gmail.com>
2026-03-12 10:44:50 +0530

    Update state.py

--- state.py
+++ state.py
@@ -4,18 +4,16 @@ class BotState:
 
         self.capital = None
         self.peak = None
-        self.max_dd = 0
 
         self.position = None
         self.entry_price = 0
         self.size = 0
-        self.entry_risk = 0
+        self.stop = 0
 
-        self.cooldown = 0
         self.last_candle_time = None
+        self.entry_candle = None
 
         self.trades = []
-        self.losing_streak = 0
 
     def initialize(self, balance):
 
